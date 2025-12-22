db3bface160b003b26d5b25efb3b12aace183da9
Ana Pantilie <ana.pantilie95@gmail.com>
2025-12-22 14:38:04 +0200

    Don't allow constant folding

--- plutus-scripts/PlutusScripts/Batch6/Value/V3_110.hs
+++ plutus-scripts/PlutusScripts/Batch6/Value/V3_110.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.1.0 #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:no-simplifier-evaluate-builtins #-}
 
 module PlutusScripts.Batch6.Value.V3_110 where
 
